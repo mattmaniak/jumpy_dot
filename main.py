1da37f0cb9e747123e06780fa31b4564fb475840
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2018-03-18 16:34:42 +0100

    Comments are more readable now.

--- main.py
+++ main.py
@@ -5,75 +5,59 @@ from random import randint
 from sys import stdout
 from time import sleep
 
-# You can set up height of the playable area to fit it.
-# It must be set up manually in models.py file.
-user_area_height = int(2)
+def environment(): # Non-playable area with clouds, above playable area.
 
-# Non-playable area with clouds, above playable area.
-def environment():
 	ui.horizontal_border()
 
-	for i in range(ui.y - user_area_height - 2): # Environment height.
-		print(ui.white + "#", end = "" + ui.default)
+	for i in range(ui.y - 4): # Environment height.
+		print(ui.white + "#", end = "" + ui.default) # Left border.
 
-		# Whitespaces after left border.
-		for i in range(ui.x - 2):
+		for i in range(ui.x - 2): # Spaces after left border.
 			print(ui.bright_blue + "#" + ui.default, end = "")
 
-# If you want check graphically environment height, replace above line with it.
-			# print(ui.green + "#" + ui.default, end = "")
+		print(ui.white + "#" + ui.default) # Right border.
 
-		# Right border:
-		print(ui.white + "#" + ui.default)
 
-# Area where Player takes activities with Enemies.
-def playable_area():
-	for i in range(user_area_height - 1): # models.Player.height
+def playable_area(): # Area where Player takes activities with Enemies.
 
-		# Left border.
-		print(ui.white + "#", end = "" + ui.default)
+	player_x = int(10) # Define Player position.
 
-		# Whitespaces after left border.
-		for i in range(ui.x - 2):
-			print(ui.space, end = "")
+	# Amount of spaces between the player and the enemy.
+	enemy_x = randint(0, ui.x - player_x - 4)
 
-		# Right border.
-		print(ui.white + "#" + ui.default)
-	
+
+	# Upper part of the playable_area.
+	print(ui.white + "#", end = "" + ui.default) # Left border.
+
+	for i in range(ui.x - 2): # Spaces after left border.
+		print(ui.space, end = "")
+
+	print(ui.white + "#" + ui.default) # Right border.
+
+
+	# Lower part of the playable_area.
 	# Left, fixed border before the Player.
 	print(ui.white + "#", end = "" + ui.default)
 
-	# Define Player position
-	player_x = int(10)
-
-	for i in range(player_x):
+	for i in range(player_x): # Spaces before the player.
 		print(ui.space, end = "")
 	
 	models.player()
 
-	# 1 is player.width:
-	enemy_x = randint(0, ui.x - player_x - 4)
-
-	# Space chars after the Player.
-
-	for i in range(enemy_x):
+	for i in range(enemy_x): # Space chars after the player.
 		print(ui.space, end = "")
 
 	models.enemy()
 
-	# Space chars after the Enemy.
-
-	# models.Player.width and models.Enemy.width are equal 1.
 	for i in range(ui.x - player_x - enemy_x - 4):
-		print(ui.space, end = "")
+		print(ui.space, end = "") # Space chars after the Enemy.
 	
-	# Right, fixed border after the Player.
-	print(ui.white + "#" + ui.default)
+	print(ui.white + "#" + ui.default) # Right, fixed border after the Player.
 	
 	ui.horizontal_border()
 
-# Example.
-while True:
+
+while True: # Example of rendering.
 	environment()
 	playable_area()
 	sleep(1)
--- models.py
+++ models.py
@@ -1,20 +1,15 @@
 import ui
-from sys import exit
 
-# Width and height must be entered manually and must be equal 1.
-# Examples of models below.
+# Size of models must be 1x1, eg. '#' - just one ASCII char.
 
 # Some 'model' placeholders here.
 
 def cloud():
-	for i in range(1):
-		print(ui.white + "#" + ui.default, end = "", flush = "True")
+	print(ui.white + "#" + ui.default, end = "", flush = "True")
 
 def enemy():
-	for i in range(1): # height
-		print(ui.red + "#" + ui.default, end = "", flush = "True")
+	print(ui.red + "#" + ui.default, end = "", flush = "True")
 
 def player():
-	for i in range(1): # height
-		print(ui.green + "#" + ui.default, end = "", flush = "True")
+	print(ui.green + "#" + ui.default, end = "", flush = "True")
 
