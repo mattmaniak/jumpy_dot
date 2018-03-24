8c7a120268146a55dc986f1665ad9f557df3c419
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2018-03-24 23:11:52 +0100

    Basic waiting for key implemented.

--- main.py
+++ main.py
@@ -3,36 +3,26 @@
 from random import randint
 from sys import exit as sys__exit
 from time import sleep
+from select import select
+from sys import stdin
 
 import assets.gfx as gfx
 import assets.window as window
 
-from sys import stdin
-from threading import Thread
-from subprocess import call
-import signal
-
 enemy_x = int(gfx.x - window.player_x - 4) # Enemy starting position.
 
-frame_break = float(0.1) # Time to render single frame.
+frame_break = float(0.5) # Time to render single frame.
 # It's also affects on the clouds and enemies speeds.
 
 def key_event():
-	global enemy_x
+	key, foo, bar = select([stdin], [], [], 1) # After key, moves faster.
 
-	key = None
+	if key:
+		print("Press")
+# Jump!
 
-	def time_pass():
-		sleep(frame_break - 0.01)
-		if key != None:
-			return 0
-
-		window.environment()
-		window.jump()
-		window.idle(enemy_x)
-
-	Thread(target = time_pass).start()
-	key = str(input())
+	else:
+		print("No keypress.")
 
 #	if key == "": # Empty input = 'Enter' key event.
 #		window.environment()
