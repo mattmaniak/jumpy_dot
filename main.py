b8a1b3bafa965729b69e9a1740aefa51d0235299
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2018-03-25 22:49:26 +0200

    Code shortened.

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ from sys import stdin
 import assets.gfx as gfx
 import assets.window as window
 
-frame_break = float(0.1) # Time to render single frame.
+frame_break = float(0.05) # Time to render single frame.
 # It's also affects on the clouds and enemies speeds.
 
-def round():
+def frame():
+	window.enemy_x -= 1
+	window.environment()
+	window.idle(window.enemy_x)
+	sleep(frame_break)
+
+def logic():
 	global enemy_x
 
 	key, foo, bar = select([stdin], [], [], frame_break) # Key event.
@@ -25,20 +31,14 @@ def round():
 			sleep(2 * frame_break) # 0.2 seconds.
 
 		else:
-			window.enemy_x -= 1
-			window.environment()
-			window.idle(window.enemy_x)
-			sleep(frame_break)
+			frame()
 
 	else: # If not clicked, print without jump.
-		window.enemy_x -= 1
-		window.environment()
-		window.idle(window.enemy_x)
-		sleep(frame_break)
+		frame()
 
 window.environment() # Initial frames.
 window.idle(window.enemy_x)
 
 while 1:
-	round()
+	logic()
 
